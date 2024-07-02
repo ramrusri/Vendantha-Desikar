38abc3ee977c08af71cdd8d390f59d437e2f356c
ramrusri <81855757+ramrusri@users.noreply.github.com>
2024-07-02 12:54:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+
+console.log("rrrrrrrr")
+//app.use(express.static("express"));
+// default URL for website
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+    //console.log(res)
+  });
+  
+const server = http.createServer(app);
+const port = 3004;
+server.listen(port);
+console.debug('Server listening on port ' + port);
